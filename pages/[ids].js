43147ce3cc4cf564356999862684ebfbdf09e6e0
eliranr <eliranr497@gmail.com>
2022-07-25 01:21:51 +0300

    adding the chat page

--- pages/[ids].js
+++ pages/[ids].js
@@ -1,8 +1,28 @@
+import {useRouter} from 'next/router';
+import { ArrowSmLeftIcon, DotsVerticalIcon } from "@heroicons/react/outline";
+
 
 
 export default function Chat() {
+  const router = useRouter();
+  const { ids } = router.query;
     return (
-      <h1>room chat</h1>
+      <div className='flex flex-col h-[100vh] bg-auto bg-center' style={{background: "url('images/bg-chat1.png')"}}>
+        <div className='flex flex-none items-center bg-green-600 text-white pr-2.5 pl-1.5 h-[48px] space-x-1.5'>
+          <ArrowSmLeftIcon className='h-7 hoverEffect' onClick={() => router.push('/')}/>
+          <div className='flex flex-grow items-center space-x-2'>
+            <img className="w-7 rounded-full" src="https://st4.depositphotos.com/11634452/21365/v/600/depositphotos_213659488-stock-illustration-picture-profile-icon-human-people.jpg" />
+            <h1 className='font-semibold'>{ids}</h1>
+          </div>
+          <DotsVerticalIcon className="h-7 hoverEffect"/>
+        </div>
+        <div className='w-full flex-grow'>
+          fdgfgfg
+        </div>
+        <div className='p-2'>
+          <input type='text' placeholder='Message' className='w-full h-8 rounded-full px-4' />
+        </div>
+      </div>
     )
 }
   
--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,32 @@
 import { SearchIcon, DotsVerticalIcon } from "@heroicons/react/outline";
+import {useRouter} from 'next/router';
 
 export default function Home() {
+  const router = useRouter();
   return (
     <div>
-      <div className="flex items-center justify-between bg-green-600 text-white text-xl py-2.5 px-2.5 font-semibold">
-      <h1>WhatsApp</h1>
-      <div className="flex flex-row space-x-1">
-        <SearchIcon className="h-7 hoverEffect"/>
-        <DotsVerticalIcon className="h-7 hoverEffect"/>
-      </div>
-    </div>
-    {chats.map(chat => 
-      <div className="flex flex-row items-center p-2 hover:bg-green-100 space-x-2 h-10">
-        <img className="w-8 rounded-full" src="https://st4.depositphotos.com/11634452/21365/v/600/depositphotos_213659488-stock-illustration-picture-profile-icon-human-people.jpg" />
-        <div className="flex flex-row items-center justify-between w-full">
-          <div className="flex flex-col justify-center space-y-0">
-            <h1 className="text-sm font-semibold leading-tight">{chat.name}</h1>
-            <span className="text-xs leading-tight">{chat.last_mes}</span>
-          </div>
-          <div className="flex fles-col">
-            <span className="text-xs">{chat.time}</span>
-          </div>
+      <div className="flex items-center justify-between bg-green-600 text-white text-xl pr-2.5 pl-3 h-[48px] font-semibold">
+        <h1>WhatsApp</h1>
+        <div className="flex flex-row space-x-1">
+          <SearchIcon className="h-7 hoverEffect"/>
+          <DotsVerticalIcon className="h-7 hoverEffect"/>
         </div>
       </div>
-    )}
+      {chats.map(chat => 
+        <div key={chat.name} onClick={() => router.push(chat.name)} className="flex flex-row items-center px-2 py-2 h-[52px] hover:bg-green-100 space-x-2  cursor-pointer">
+          <img className="w-8 rounded-full" src="https://st4.depositphotos.com/11634452/21365/v/600/depositphotos_213659488-stock-illustration-picture-profile-icon-human-people.jpg" />
+          <div className="flex flex-row items-center justify-between w-full">
+            <div className="flex flex-col justify-center">
+              <h1 className="text-sm font-semibold leading-3">{chat.name}</h1>
+              <span className="text-xs leading-tight">{chat.last_mes}</span>
+            </div>
+            <div className="flex flex-col items-end space-y-1 h-full">
+              <span className="text-xs">{chat.time}</span>
+              {chat.state > 0 && <span className="text-[10px] bg-green-500 rounded-full w-4 h-4 flex items-center justify-center text-white">{chat.state}</span>}
+            </div>
+          </div>
+        </div>
+      )}
     </div>
     
     
